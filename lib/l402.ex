74d6c6a980455442dce1cb808d290450be589076
jurraca <julienu@pm.me>
2023-07-23 02:08:01 +0200

    move main API to L402 module, fix plug impl

--- lib/l402.ex
+++ lib/l402.ex
@@ -1,18 +1,53 @@
 defmodule L402 do
   @moduledoc """
-  Documentation for `L402`.
+    An implementation of the L402 spec as a Plug.
+    A user must provide a macaroon (a fancy cookie) and a proof of payment (a preimage) which uniquely identifies the service and the capabilities the service provides to the user.
+    We gate a given resource with a Lightning payment. If the user can provide a proof of payment for an invoice, they get access.
+    Flow works like this:
+      - user hits the resource.
+      - The service checks its `authorization` header for a valid L402 header.
+      - If the user doesn't have one, we create the L402 and set the `www-authenticate` header to `L402 macaroon:preimage`.
+      - The service returns this header with a 402 - Payment Required status code.
+      - The client receives this response and handles it. The frontend could show the user this invoice to pay, or, if the user has a webLN-enabled extension, the invoice can be paid automatically within that context.
+      - The user pays the invoice and gets a "proof of payment": the preimage.
+      - The user supplies that proof of payment in a second request to the resource.
+      - The service validates the proof of payment, and the user gets access to the service.
+
+    `build_challenge/2` is the main entrypoint.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> L402.hello()
-      :world
-
+    Builds a L402 challenge from an invoice amount and `opts`. See `Lnrpc.Invoice` for a full list of options to pass.
   """
-  def hello do
-    :world
+  def build_challenge(invoice_amount, opts \\ []) do
+
+    with {:ok, channel} <- get_channel(),
+      %{payment_request: invoice} <- invoice_request(channel, invoice_amount, opts),
+      {:ok, %{payment_hash: payment_hash}} <- LNUtils.decode_invoice(invoice) do
+    {:ok, token} = Macaroons.build([caveats: [payment_hash: payment_hash]])
+    l402 = "L402 token=" <> token <> " invoice=" <> invoice
+    {:ok, {token, l402}}
+    else
+        {:error, msg} -> Logger.error(msg)
+        err -> err
+    end
+  end
+
+  @doc """
+    Request an invoice for the `amount` from a Lightning node connected via the `channel`.
+  """
+  def invoice_request(channel, invoice_amount, opts) do
+    case LND.create_invoice(channel, invoice_amount, opts) do
+      {:ok, body = %Lnrpc.AddInvoiceResponse{}} -> body
+      {:error, msg} ->
+        {:error, "could not fetch invoice from LND"}
+    end
+  end
+
+  defp get_channel() do
+    case Channel.get(:channel) do
+      {:ok, nil} -> Channel.connect()
+      {:ok, chan} -> {:ok, chan}
+    else
   end
 end
--- lib/l402/plug.ex
+++ lib/l402/plug.ex
@@ -1,10 +1,6 @@
-defmodule L402.Plugs.Main do
+defmodule L402.Plug do
   @moduledoc """
-      Plug to handle an LSAT-compliant authentication flow.
-      If no mac, instruct LND to create one.
-      Respond to client with mac and invoice in authenticate header.
-      Client pays invoice, receives preimage.
-      User responds with mac and preimage via Authorization field.
+    A Plug to handle an L402-compliant authentication flow.
   """
   alias Plug.Conn
   alias L402.GRPCChannel, as: Channel
@@ -16,56 +12,28 @@ defmodule L402.Plugs.Main do
   def init(opts), do: opts
 
   @doc """
-  Matches on the authorization header of the form Authorization "L402 <macaroon>:<preimage>.
-  No Authorization header.
-  Set WWW-Authenticate: "L402 macaroon=<mac>, invoice=<invoice>"
+  Matches on the authorization header of the form Authorization "L402 <macaroon>:<preimage>".
+  If no Authorization header is found, we issue a macaroon and a Lightning invoice, with a 402 response.
+  The plug expects an "amount" in the assigns: you should set this value in your controller for the resource calling this plug.
+  Default is to forbid access.
   """
-  def call(%Plug.Conn{req_headers: [{"authorization", auth}]} = conn) do
-    IO.inspect(auth, label: "Auth, clause 1")
-    validate_402(auth)
-    conn
-  end
-
-  def call(%Plug.Conn{params: %{"L402" => l402}} = conn, _opts) do
-    case validate_402(l402) |> IO.inspect() do
+  def call(%Plug.Conn{req_headers: [{"authorization", auth}]} = conn, _opts) do
+    case validate_402(l402) do
         {:ok, _} ->
-            conn
-            |> Conn.put_req_header("authorization", "L402")
-            |> Conn.put_status(200)
-        _ -> conn |> call([])
+          Conn.put_status(conn, 200)
+        _ -> Conn.put_status(conn, :forbidden)
     end
   end
 
-  def call(%Plug.Conn{} = conn, _opts) do
-    {token, l402} = build_challenge()
+  def call(%Plug.Conn{assigns: %{amount: amount}} = conn, _opts) do
+    {macaroon, l402} = build_challenge(amount)
     conn
-    |> Conn.put_resp_cookie("my-cookie", token)
+    #|> Conn.put_resp_cookie("my-cookie", macaroon)
     |> Conn.put_resp_header("www-authenticate", l402)
     |> Conn.put_status(:payment_required)
   end
 
-  def call(conn, _opts) do
-    conn
-  end
-
-  def build_challenge(invoice_amount) do
-    with %{channel: channel} <- Channel.connect(),
-      %{payment_request: invoice} <- invoice_request(channel, invoice_amount),
-      {:ok, %{payment_hash: payment_hash}} <- LNUtils.decode_invoice(invoice) do
-    {:ok, token} = Macaroons.build([caveats: [payment_hash: payment_hash]])
-    l402 = "L402 token=" <> token <> " invoice=" <> invoice
-    {token, l402}
-    else
-        error -> Logger.error(error)
-    end
-  end
-
-  defp invoice_request(channel, invoice_amount) do
-    case LND.create_invoice(channel, invoice_amount) do
-      {:ok, body = %Lnrpc.AddInvoiceResponse{}} -> body
-      msg -> msg
-    end
-  end
+  def call(conn, _opts), do: Conn.put_status(:forbidden)
 
   def validate_402("L402 " <> rest) do
     [macaroon, preimage] = String.split(rest, ":")
@@ -84,13 +52,5 @@ defmodule L402.Plugs.Main do
 
   def validate_402(data) do
     {:error, "Wrong format for L402 header, got #{data}"}
-  end
-
-  defp get_payment_hash_from_jwt(jwt) do
-    case Token.verify_and_validate(jwt) do
-        {:ok, %{payment_hash: hash}} -> {:ok, hash}
-        {:ok, _} -> {:error, "JWT must contain payment hash"}
-        _ -> {:error, "Invalid JWT"}
-    end
   end
 end

476a6b8f9b469d9e874583dafc0a2178c2028ec3
jurraca <julienu@pm.me>
2023-07-26 01:46:48 +0200

    improve implementation and fix broken control flow

--- lib/l402.ex
+++ lib/l402.ex
@@ -1,29 +1,21 @@
 defmodule L402 do
   @moduledoc """
-    An implementation of the L402 spec as a Plug.
-    A user must provide a macaroon (a fancy cookie) and a proof of payment (a preimage) which uniquely identifies the service and the capabilities the service provides to the user.
-    We gate a given resource with a Lightning payment. If the user can provide a proof of payment for an invoice, they get access.
-    Flow works like this:
-      - user hits the resource.
-      - The service checks its `authorization` header for a valid L402 header.
-      - If the user doesn't have one, we create the L402 and set the `www-authenticate` header to `L402 macaroon:preimage`.
-      - The service returns this header with a 402 - Payment Required status code.
-      - The client receives this response and handles it. The frontend could show the user this invoice to pay, or, if the user has a webLN-enabled extension, the invoice can be paid automatically within that context.
-      - The user pays the invoice and gets a "proof of payment": the preimage.
-      - The user supplies that proof of payment in a second request to the resource.
-      - The service validates the proof of payment, and the user gets access to the service.
-
+    Minimum public API for services to interact with the protocol.
     `build_challenge/2` is the main entrypoint.
   """
 
   alias L402.GRPCChannel, as: Channel
+  alias L402.{Macaroons, Server}
   alias Bitcoinex.LightningNetwork, as: LNUtils
+
+
+  require Logger
 
   @doc """
     Builds a L402 challenge from an invoice amount and `opts`. See `Lnrpc.Invoice` for a full list of options to pass.
   """
   def build_challenge(invoice_amount, opts \\ []) do
-    with %{payment_request: invoice} <- invoice_request(invoice_amount, opts),
+    with %{payment_request: invoice} <- request_invoice(invoice_amount, opts),
       {:ok, %{payment_hash: payment_hash}} <- LNUtils.decode_invoice(invoice) do
     {:ok, token} = Macaroons.build([caveats: [payment_hash: payment_hash]])
     l402 = "L402 token=" <> token <> " invoice=" <> invoice
@@ -38,9 +30,9 @@ defmodule L402 do
     Request an invoice for the `amount` from a Lightning node connected via the `channel`.
   """
   def request_invoice(invoice_amount, opts) do
-    case get_channel() |> LND.create_invoice(invoice_amount, opts) do
+    case get_channel() |> Server.create_invoice(invoice_amount, opts) do
       {:ok, body = %Lnrpc.AddInvoiceResponse{}} -> body
-      {:error, msg} ->
+      {:error, _msg} ->
         {:error, "could not fetch invoice from LND"}
     end
   end
--- lib/l402/plug.ex
+++ lib/l402/plug.ex
@@ -1,55 +1,62 @@
 defmodule L402.Plug do
   @moduledoc """
     A Plug to handle an L402-compliant authentication flow.
+
+    The Plug matches on the authorization header of the form "L402 <macaroon>:<preimage>".
+    If no authorization header is found, a 402 response is returned with a token and a Lightning invoice.
+    The plug expects an "amount" in the assigns, as issuing a payment invoice would not be possible without it. You should set this value in your controller or router.
   """
   alias Plug.Conn
-  alias L402.Server, as: LND
   alias L402.Macaroons
   require Logger
 
   def init(opts), do: opts
 
-  @doc """
-  Matches on the authorization header of the form Authorization "L402 <macaroon>:<preimage>".
-  If no Authorization header is found, we issue a macaroon and a Lightning invoice, with a 402 response.
-  The plug expects an "amount" in the assigns: you should set this value in your controller for the resource calling this plug.
-  Default is to forbid access.
-  """
-  def call(%Plug.Conn{req_headers: [{"authorization", auth}]} = conn, _opts) do
-    case validate_402(l402) do
+
+  def call(%Plug.Conn{req_headers: [{"authorization", "L402 " <> auth}]} = conn, _opts) do
+    case validate_402(auth) do
         {:ok, _} ->
           Conn.put_status(conn, 200)
-        _ -> Conn.put_status(conn, :forbidden)
+        _ -> Conn.put_status(conn, :unauthorized)
     end
   end
 
+  # If the conn has an amount but no authorization, return a 402 with challenge.
   def call(%Plug.Conn{assigns: %{amount: amount}} = conn, _opts) do
-    {macaroon, l402} = L402.build_challenge(amount)
-    conn
-    #|> Conn.put_resp_cookie("my-cookie", macaroon)
-    |> Conn.put_resp_header("www-authenticate", l402)
-    |> Conn.put_status(:payment_required)
-  end
-
-  def call(conn, _opts), do: Conn.put_status(conn, :forbidden)
-
-  def validate_402("L402" <> rest), do: validate_402(rest)
-  def validate_402(rest) do
-    [macaroon, preimage] = String.split(rest, ":")
-    binary_mac = Base.url_decode64(macaroon)
-    hashed_preimage = :crypto.hash(preimage, :sha256)
-    case Macaroons.get_payment_hash_from_macaroon(binary_mac) do
-        {:ok, payment_hash} ->
-          if payment_hash != hashed_preimage do
-            {:error, "Preimage doesn't match provided payment_hash"}
-          else
-            Macaroons.verify_caveats(binary_mac, payment_hash)
-          end
-        {:error, _} = err -> err
+    case L402.build_challenge(amount) do
+    {:ok, {_token, l402}} -> conn
+      |> Conn.put_resp_header("www-authenticate", l402)
+      |> Conn.put_status(:payment_required)
+    {:error, _} -> Conn.put_status(conn, :service_unavaible)
     end
   end
 
-  def validate_402(data) do
-    {:error, "Wrong format for L402 header, got #{data}"}
+  # The upstream pipeline must provide an amount to build a challenge from.
+  def call(conn, _opts) do
+    Conn.put_status(conn, :bad_request)
+  end
+
+  @doc """
+  Parse an L402 header. The macaroon is a base64 encoded bearer token. The preimage is hex-encoded.
+  In order for the authorization to be valid, the payment hash included in the macaroon issued by the server must match the sha256 hash of the proof of payment provided by the client.
+  """
+  def validate_402("L402 " <> rest), do: validate_402(rest)
+  def validate_402(rest) do
+    with [macaroon, preimage] <- String.split(rest, ":"),
+      {:ok, binary_mac} <- Base.url_decode64(macaroon) do
+      hashed_preimage = :crypto.hash(preimage, :sha256)
+
+      case Macaroons.get_payment_hash_from_macaroon(binary_mac) do
+          {:ok, payment_hash} ->
+            if payment_hash != hashed_preimage do
+              {:error, "Preimage doesn't match provided payment_hash"}
+            else
+              Macaroons.verify_caveats(binary_mac, payment_hash)
+            end
+          {:error, _} = err -> err
+      end
+    else
+      _err -> {:error, "Wrong format for L402 header, got #{rest}"}
+    end
   end
 end

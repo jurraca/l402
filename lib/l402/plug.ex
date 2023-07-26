9a4dfa3488ed504f7e613b630603fda7d1c7def5
jurraca <julienu@pm.me>
2023-07-26 03:51:51 +0200

    improve 402 validation

--- lib/l402/plug.ex
+++ lib/l402/plug.ex
@@ -8,26 +8,31 @@ defmodule L402.Plug do
   """
   alias Plug.Conn
   alias L402.Macaroons
+  alias Macaroon.Types.Macaroon
   require Logger
 
   def init(opts), do: opts
 
-
   def call(%Plug.Conn{req_headers: [{"authorization", "L402 " <> auth}]} = conn, _opts) do
     case validate_402(auth) do
-        {:ok, _} ->
-          Conn.put_status(conn, 200)
-        _ -> Conn.put_status(conn, :unauthorized)
+      {:ok, _} ->
+        Conn.put_status(conn, 200)
+
+      _ ->
+        Conn.put_status(conn, :unauthorized)
     end
   end
 
   # If the conn has an amount but no authorization, return a 402 with challenge.
   def call(%Plug.Conn{assigns: %{amount: amount}} = conn, _opts) do
     case L402.build_challenge(amount) do
-    {:ok, {_token, l402}} -> conn
-      |> Conn.put_resp_header("www-authenticate", l402)
-      |> Conn.put_status(:payment_required)
-    {:error, _} -> Conn.put_status(conn, :service_unavaible)
+      {:ok, {_token, l402}} ->
+        conn
+        |> Conn.put_resp_header("www-authenticate", l402)
+        |> Conn.put_status(:payment_required)
+
+      {:error, _} ->
+        Conn.put_status(conn, :service_unavaible)
     end
   end
 
@@ -41,22 +46,26 @@ defmodule L402.Plug do
   In order for the authorization to be valid, the payment hash included in the macaroon issued by the server must match the sha256 hash of the proof of payment provided by the client.
   """
   def validate_402("L402 " <> rest), do: validate_402(rest)
+
   def validate_402(rest) do
     with [macaroon, preimage] <- String.split(rest, ":"),
-      {:ok, binary_mac} <- Base.url_decode64(macaroon) do
+         {:ok, binary_mac} <- Base.url_decode64(macaroon) do
       hashed_preimage = :crypto.hash(preimage, :sha256)
 
-      case Macaroons.get_payment_hash_from_macaroon(binary_mac) do
-          {:ok, payment_hash} ->
-            if payment_hash != hashed_preimage do
-              {:error, "Preimage doesn't match provided payment_hash"}
-            else
-              Macaroons.verify_caveats(binary_mac, payment_hash)
-            end
-          {:error, _} = err -> err
+      case Macaroons.decode(binary_mac) do
+        {:ok, decoded} -> validate_challenge(decoded, hashed_preimage)
+        {:error, _} = err -> err
       end
     else
       _err -> {:error, "Wrong format for L402 header, got #{rest}"}
+    end
+  end
+
+  defp validate_challenge(%Macaroon{payment_hash: payment_hash, caveats: caveats}, client_hash) do
+    if payment_hash != client_hash do
+      {:error, "Preimage doesn't match provided payment hash"}
+    else
+      Macaroons.verify_payment_hash(caveats, payment_hash)
     end
   end
 end

ab59098476760dbd7340176c21fcaf0edaa4c9d2
jurraca <julienu@pm.me>
2023-07-31 14:56:52 +0200

    remove Joken-related module, add config

---
+++ config/config.exs
@@ -0,0 +1,11 @@
+import Config
+
+config :l402,
+    admin_macaroon_path: System.get_env("ADMIN_MAC"),
+    cert_path: System.get_env("CERT_PATH")
+
+config :grpc,
+  host: System.get_env("HOST") || "127.0.0.1",
+  port: System.get_env("PORT") || 10009
+
+#import_config "#{config_env()}.exs"
